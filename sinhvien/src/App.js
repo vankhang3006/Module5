6c1c346a0068a72a300c939f3f14df15380975ba
vankhang3006 <119319324+vankhang3006@users.noreply.github.com>
2023-09-11 20:27:39 +0700

    ss2sda

---
+++ sinhvien/src/App.js
@@ -0,0 +1,51 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+      <table>
+        {students.map(student => (
+            <tr>
+              <td>{student.company}</td>
+              <td>{student.contact}</td>
+              <td>{student.country}</td>
+            </tr>
+        ))}
+      </table>
+  );
+}
+
+const students = [
+  {
+    company: 'Alfreds Futterkiste',
+    contact: 'Maria Anders',
+    country: 'Germany'
+  },
+  {
+    company: 'Centro comercial Moctezuma',
+    contact: 'Francisco Chang',
+    country: 'Mexico'
+  },
+  {
+    company: 'Ernst Handel',
+    contact: 'Roland Mendel',
+    country: 'Austria'
+  },
+  {
+    company: 'Island Trading',
+    contact: 'Helen Bennett',
+    country: 'UK'
+  },
+  {
+    company: 'Laughing Bacchus Winecellars',
+    contact: 'Yoshi Tannamuri',
+    country: 'Canada'
+  },
+  {
+    company: 'Magazzini Alimentari Riuniti',
+    contact: 'Giovanni Rovelli',
+    country: 'Italy'
+  }
+]
+
+export default App;
---
+++ sinhvien/src/index.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+const students = [
+    {
+        company: 'Alfreds Futterkiste',
+        contact: 'Maria Anders',
+        country: 'Germany'
+    },
+    {
+        company: 'Centro comercial Moctezuma',
+        contact: 'Francisco Chang',
+        country: 'Mexico'
+    },
+    {
+        company: 'Ernst Handel',
+        contact: 'Roland Mendel',
+        country: 'Austria'
+    },
+    {
+        company: 'Island Trading',
+        contact: 'Helen Bennett',
+        country: 'UK'
+    },
+    {
+        company: 'Laughing Bacchus Winecellars',
+        contact: 'Yoshi Tannamuri',
+        country: 'Canada'
+    },
+    {
+        company: 'Magazzini Alimentari Riuniti',
+        contact: 'Giovanni Rovelli',
+        country: 'Italy'
+    }
+]
+{students.map(students =>
+    <tr>
+        <td>{students.company}</td>
+        <td>{students.contact}</td>
+        <td>{students.country}</td>
+    </tr>
+)}
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
