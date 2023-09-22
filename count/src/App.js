1a7d612c87132a285a80275699312b92be674510
vankhang3006 <119319324+vankhang3006@users.noreply.github.com>
2023-09-22 18:49:58 +0700

    fix

--- count/src/App.js
+++ count/src/App.js
@@ -1,34 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-import {useState} from 'react'
-function App() {
-  const [count, setCount] = useState(0);
-  const [number, setNumber] = useState(0);
+import React, { useState } from 'react';
 
-    const handleClick = () => {
-    const newValue = count + 1;
-    setCount(newValue);
-  };
-    const handleAdd = () => {
-        const newNumber = number + 2;
-        setNumber(newNumber);
-    };
-  return (
-      <>
-          <div>
-              Count: {count}
-              <div>
-                  <button onClick={handleClick}>Add 1</button>
-              </div>
-          </div>
-          <div>
-              Count: {number}
-              <div>
-                  <button onClick={handleAdd}>Add 2</button>
-              </div>
-          </div>
-      </>
-  );
+function useIncrement(addAmount) {
+    const [count, setCount] = useState(0);
+
+    function increase() {
+        setCount(count + addAmount);
+    }
+
+    return [count, increase];
+}
+
+function Counter({ addAmount }) {
+    const [count, increase] = useIncrement(addAmount);
+
+    return (
+        <div>
+            <p>Count: {count}</p>
+            <button onClick={increase}>Add {addAmount}</button>
+        </div>
+    );
+}
+
+function App() {
+    return (
+        <div className="App">
+            <Counter addAmount={1} />
+            <Counter addAmount={2} />
+        </div>
+    );
 }
 
 export default App;
